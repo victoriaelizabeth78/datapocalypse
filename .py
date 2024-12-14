2026e76c61fc107980253ec59bfb88d858f29690
king-quantum <quantumcoder134@gmail.com>
2024-12-14 11:00:43 +0000

    Create .py

---
+++ .py
@@ -0,0 +1,18 @@
+def guessing_game(max):
+    '''
+    A guessing game where you will try to guess the integer  generated between 0 and the value you gave the function
+    :param max: the highest limit of the game
+    :return: None
+    '''
+    target = rn.randint(0, max)
+    guess = max + 1
+    counter = 0
+    while target != guess:
+        counter += 1
+        guess = int(input("What number would you like to guess?: "))
+        if guess > target:
+            print("Guess too high")
+        elif guess < target:
+            print("Guess too low")
+
+    print(f"Good job, you got the right number in {counter} guesses!")
